4ff46621a708ecbe7a16f14159b91ce13d20ae38
John Jones <JonesJohnM@JohnDeere.com>
2021-01-22 23:09:22 -0600

    chore: cleanup imports

--- src/main/java/com/jones2026/springhello/Greetings/GreetingController.java
+++ src/main/java/com/jones2026/springhello/Greetings/GreetingController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Mono;
 
 @Controller
--- src/test/java/com/jones2026/springhello/Greetings/GreetingControllerTests.java
+++ src/test/java/com/jones2026/springhello/Greetings/GreetingControllerTests.java
@@ -1,26 +1,16 @@
 package com.jones2026.springhello.Greetings;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.jones2026.springhello.Application;
 import org.junit.jupiter.api.Test;
-
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
+
 import reactor.core.publisher.Mono;
+
 @WebFluxTest(GreetingController.class)
 public class GreetingControllerTests {
 
@@ -35,11 +25,7 @@ public class GreetingControllerTests {
     public void greetingShouldReturnMessageFromService() throws Exception {
         when(service.greet(anyString())).thenReturn(GREETING_SERVICE_CALLED);
 
-        webTestClient
-                .get().uri("/greeting?name=Mock")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
-                .value( v -> v.equalsIgnoreCase(GREET));
+        webTestClient.get().uri("/greeting?name=Mock").exchange().expectStatus().isOk().expectBody(String.class)
+                .value(v -> v.equalsIgnoreCase(GREET));
     }
 }
